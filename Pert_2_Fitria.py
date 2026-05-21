b6102544996e6274c8de2d6b10fc7b8a0b7532e8
rachmadanieka11-gif <rachmadanieka11@gmail.com>
2026-05-21 23:14:57 +0700

    Add files via upload
    First commit

---
+++ Pert_2_Fitria.py
@@ -0,0 +1,79 @@
+#1
+print("halo")
+print("Saya Fitria")
+
+#2
+print(11_111_111)
+print(-11_111_111)
+
+#3
+print(0o173)
+print(0*345)
+
+#4
+print(0.4)
+print(0.3)
+print(4.)
+print(.3)
+print(2.5e2)
+print(4e-1)
+
+#5
+print("Halo, saya fitria eka rachmadani")
+print('saya fitria eka')
+
+#6
+print(15<30)
+print(30<=15)
+
+#codingan 7
+print(2 ** 3)
+print(2 ** 3.0)
+print(2.0 ** 3)
+print(3.5 ** 4.5)
+
+#codingan 8
+print(2 * 3)
+print(2 * 3.0)
+print(2.0 * 3)
+print(3.5 * 4.5) 
+
+#codingan 9
+print(4/2)
+print(2/2.0)
+print(3.0/3)
+print(3.0/2.5)
+
+#codingan 10
+print(6//2)
+print(2//2.0)
+print(6.0//3)
+print(3.0//2.5)
+
+#codingan 11
+print(4%2)
+print(2%2.0)
+print(3.0%3)
+print(3.0%2.5)
+
+#codingan 12
+#operator unary
+print(+5)
+print(-4)
+#operator binary 
+print(3+4)
+print(3*5)
+print(4/2)
+print(5-3)
+print(5**3)
+
+#codingan 13
+#Subekspresi
+print((2+3)*4)
+print((50-24)/(30+5))
+
+#codingan 14
+#kuis
+print((2 ** 4), (2 * 4.), (2 * 4))
+print((-2/4), (2/4), (2//4), (-2//4))
+print((2%-4), (2%4.), (2 ** 3 ** 2))
