85cafe80a271671fa1becde7e94f6d755265418d
rachmadanieka11-gif <rachmadanieka11@gmail.com>
2026-05-21 23:16:16 +0700

    Add files via upload
    First commit

---
+++ Pert_3_Fitria.py
@@ -0,0 +1,60 @@
+#kode 1
+var = 1
+nilai  = 98
+nama_mahasiswa = "Naladipa"
+print(var)
+print(nilai)
+print(nama_mahasiswa)
+print(var, nilai, nama_mahasiswa)
+
+#kode 2
+nama = "Budi"
+print(nama)
+
+#kode 3
+umur = "20"
+print(umur)
+umur = 20 + 1
+print(umur)
+
+#kode 4
+tabungan = 5000
+tabungan = 5000 + 6000
+print(tabungan)
+
+#kode 5
+x=2
+x *= 2
+sheep=2
+sheep *= 30
+print(x,sheep)
+
+#kode 6
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 4) ** 0.5
+print("c =", c)
+
+#kode 7
+ayu 100000
+bagus = 400000
+citra = 300000
+print(f"{ayu},{bagus},{citra}")
+jumlahTabungan = ayu + bagus + citra
+print(f"jumlah tabungan adalah {jumlahTabungan}")
+
+#kuis 4
+kilometers = 12.25
+miles = 7.38
+
+miles_kilometers = 1.3123123
+kilometers_to_miles = 1.3123123
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+#kuis 5
+x = 0
+x float(x)
+y = 3*x**3 - 2*x**2 + 3*x-1
+print("y =", y)
